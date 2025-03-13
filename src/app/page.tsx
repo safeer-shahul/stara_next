8545871d56ef491c9d80b16fb3446978e0f8381c
Safeer Shahul <safeershahul.cv@gmail.com>
2025-03-13 09:29:02 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import HeroSlider from '@/components/HeroSlider';
 import ShopLayout from './shop/layout'; // Import ShopLayout
+import CategoryGrid from '@/components/CategoryGrid';
+import FashionPortraitSlider from '@/components/FashionPortraitSlider';
+import ProductSlider from '@/components/ProductSlider';
 
 export default function Home() {
   return (
     <ShopLayout>
       <HeroSlider/>
+      <CategoryGrid/>
+      <FashionPortraitSlider/>
+      <ProductSlider/>
     </ShopLayout>
   );
 }
