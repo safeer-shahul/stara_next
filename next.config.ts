b8750a2d5ba7730df9103325af55b4e498e53d23
safeer <safeer.shahul@htpl.cc>
2025-07-02 22:20:21 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['127.0.0.1', 'localhost'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if ESLint errors are present
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if TypeScript errors are present
+    ignoreBuildErrors: true,
+  },
+  output: 'standalone',
 };
 
 export default nextConfig;
