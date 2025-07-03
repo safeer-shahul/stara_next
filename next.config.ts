20ade21f28b77deef8d0d3d7bca5417c9cfe9293
safeer <safeer.shahul@htpl.cc>
2025-07-03 13:13:27 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['127.0.0.1', 'localhost'],
+    domains: ['127.0.0.1', 'localhost','apitest.starajewels.com'],
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if ESLint errors are present
